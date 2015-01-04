0c92562ad4d7e1bf61afbb00004f4ba0ef545278
JrSchild <joriami@gmail.com>
2015-01-04 14:51:32 +0100

    Refactor the code into a Lazypipe class that can be instantiated.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function validateStep(step) {
     throw new Error('Invalid call to lazypipe().pipe(): no stream creation function specified');
   } else if (typeof step !== 'function') {
     throw new Error('Invalid call to lazypipe().pipe(): argument is not a function.\n' +
-                      '    Remember not to call stream creation functions directly! e.g.: pipe(foo), not pipe(foo())');
+                    '    Remember not to call stream creation functions directly! e.g.: pipe(foo), not pipe(foo())');
   }
 }
 
@@ -18,36 +18,31 @@ function validateSteps(steps) {
   }
 }
 
-function createPipeline(steps) {
-  function Pipeline() {
-    validateSteps(steps);
+function Lazypipe(steps) {
+  this.steps = steps;
+  this.go = this.go.bind(this);
+  this.go.pipe = this.pipe.bind(this);
 
-    return combine.apply(null, steps.map(function (t) {
-      return t.task.apply(null, t.args);
-    }));
-  }
-  
-  Pipeline.appendStepsTo = function (otherSteps) {
-    return otherSteps.concat(steps);
-  };
-  
-  Pipeline.pipe = function (step) {
-    validateStep(step);
-
-    // avoid creating nested pipelines
-    if (step.appendStepsTo) {
-      return createPipeline(step.appendStepsTo(steps));
-    }
-
-    return createPipeline(steps.concat({
-      task: step,
-      args: Array.prototype.slice.call(arguments, 1)
-    }));
-  };
-  
-  return Pipeline;
+  return this.go;
 }
 
+Lazypipe.prototype.pipe = function (step) {
+  validateStep(step);
+
+  return new Lazypipe(this.steps.concat({
+    task: step,
+    args: Array.prototype.slice.call(arguments, 1)
+  }));
+};
+
+Lazypipe.prototype.go = function () {
+  validateSteps(this.steps);
+
+  return combine.apply(null, this.steps.map(function (step) {
+    return step.task.apply(null, step.args);
+  }));
+};
+
 module.exports = function () {
-	return createPipeline([]);
+  return new Lazypipe([]);
 };
